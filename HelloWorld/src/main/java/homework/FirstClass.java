5a266185ef49a61548cf41c199f3ed1ec51f7f79
Martins-K <martins.kruklis@gmail.com>
2022-05-13 09:08:55 +0300

    Added homework

--- HelloWorld/src/main/java/homework/FirstClass.java
+++ HelloWorld/src/main/java/homework/FirstClass.java
@@ -1,4 +1,22 @@
 package homework;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        //Main metodē vajag nodefinēt mainīgos (ar atbilstošo datatipu) Valstij (brīva izvēle)
+        String countryName = "Latvia";
+        int countryPopulation = 1850000;
+        int countryAreaKm2 = 64589;
+        String countryCapital = "Riga";
+        String countryOfficalLanguage = "Latvian";
+        Boolean isCountryEuMember = true;
+        char countryCurrency = 'E';
+        System.out.printf("Hello! My name is Martins and I live in %s. The capital of %s is %s and the offical language is %s.\n",countryName, countryName, countryCapital, countryOfficalLanguage);
+        System.out.printf("The population of %s in 2022 was %d people and the total area is %d km².\n",countryName, countryPopulation, countryAreaKm2);
+        if (isCountryEuMember) {
+            System.out.println("Latvia is a EU member state!");
+        } else {
+            System.out.println("Latvia is not a EU member state!");
+        }
+        System.out.printf("The population density of Latvia is %f", countryPopulation/countryAreaKm2);
+    }
 }
