df2350abf7e85055d6c54128f084a7121b01135c
Martins-K <martins.kruklis@gmail.com>
2022-05-13 09:30:22 +0300

    updated homework

--- HelloWorld/src/main/java/homework/FirstClass.java
+++ HelloWorld/src/main/java/homework/FirstClass.java
@@ -10,13 +10,18 @@ public class FirstClass {
         String countryOfficalLanguage = "Latvian";
         Boolean isCountryEuMember = true;
         char countryCurrency = 'E';
-        System.out.printf("Hello! My name is Martins and I live in %s. The capital of %s is %s and the offical language is %s.\n",countryName, countryName, countryCapital, countryOfficalLanguage);
+        System.out.printf("Hello! My name is Martins and I live in %s. The capital of %s is %s and the official language is %s.\n",countryName, countryName, countryCapital, countryOfficalLanguage);
         System.out.printf("The population of %s in 2022 was %d people and the total area is %d km².\n",countryName, countryPopulation, countryAreaKm2);
         if (isCountryEuMember) {
             System.out.println("Latvia is a EU member state!");
         } else {
             System.out.println("Latvia is not a EU member state!");
         }
-        System.out.printf("The population density of Latvia is %f", countryPopulation/countryAreaKm2);
+        float populationDensity = countryPopulation/countryAreaKm2;
+        System.out.printf("The population density of Latvia is %.2f persons per km² \n", populationDensity);
+
+        System.out.printf("Number \"4\" multiplied by \"5\" is %d", 4*5);
+        System.out.printf("Number \"4\" divided by \"5\" with two decimal point precision is %.2f", 4/5);
+
     }
 }
